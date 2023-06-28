10c384bbc1726b5144a890b4665cca78fe156e78
Giovani Fernandes <giovanifernandes@tjro.jus.br>
2023-06-27 23:01:43 -0400

    print Buzz for multiples of 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,9 @@
 class FizzBuzz
 
   def self.print(input)
-    return "Fizz" if input % 3 == 0
-    
+    return "Fizz" if input % 3 == 0 && input % 5 != 0
+    return "Buzz" if input % 5 == 0
+
     input
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -6,14 +6,10 @@ describe FizzBuzz do
       it "should be eq 1" do
         expect(FizzBuzz.print(1)).to eq(1)
       end
-  
-      it "should be eq 2" do
-        expect(FizzBuzz.print(2)).to eq(2)
-      end
 
-      it "should be eq value 1 up to 100 skipping multiple of 3" do
+      it "should be eq value 1 up to 100 skipping multiple of 3 and 5" do
         1.upto(100) do |value|
-          next if value % 3 == 0
+          next if value % 3 == 0 || value % 5 == 0
 
           expect(FizzBuzz.print(value)).to eq(value)
         end
@@ -27,7 +23,23 @@ describe FizzBuzz do
 
       it "should be eq Fizz" do
         3.step(100, 3) do |value|
+          next if value % 5 == 0
+
           expect(FizzBuzz.print(value)).to eq("Fizz")
+        end
+      end
+    end
+
+    context "should return Buzz if it's a multiple of 5" do
+      it "should be eq Buzz" do
+        expect(FizzBuzz.print(5)).to eq("Buzz")
+      end
+
+      it "should be eq Buzz" do
+        5.step(100, 5) do |value|
+          next if value % 3 == 0
+
+          expect(FizzBuzz.print(value)).to eq("Buzz")
         end
       end
     end
