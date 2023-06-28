e91240f6e2b1bbc31cc22c3a5936121557eb7b8a
Giovani Fernandes <giovanifernandes@tjro.jus.br>
2023-06-27 23:39:25 -0400

    refactored tests

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,58 +1,62 @@
 require "fizz_buzz"
 
 describe FizzBuzz do
-  describe "Print" do
-    context "should print only number" do
-      it "should be eq 1" do
+  describe ".print" do
+    context "when input is not a multiple of 3 or 5" do
+      it "should be eq input numbers" do
         expect(FizzBuzz.print(1)).to eq(1)
+        expect(FizzBuzz.print(2)).to eq(2)
+        expect(FizzBuzz.print(4)).to eq(4)
       end
 
-      it "should be eq value 1 up to 100 skipping multiple of 3 and 5" do
-        1.upto(100) do |value|
-          next if value % 3 == 0 || value % 5 == 0
-
+      it "should be eq input numbers from 1 to 100 skipping multiples of 3 and 5" do
+        expected_output = (1..100).reject { |n| n % 3 == 0 || n % 5 == 0 }
+        expected_output.each do |value|
           expect(FizzBuzz.print(value)).to eq(value)
         end
       end
     end
 
-    context "should return Fizz if it's a multiple of 3 skipping multiple of 5" do
+    context "when input is a multiple of 3 and not a multiple of 5" do
       it "should be eq Fizz" do
         expect(FizzBuzz.print(3)).to eq("Fizz")
+        expect(FizzBuzz.print(6)).to eq("Fizz")
+        expect(FizzBuzz.print(96)).to eq("Fizz")
       end
 
-      it "should be eq Fizz" do
-        3.step(100, 3) do |value|
-          next if value % 5 == 0
-
+      it "should be eq Fizz for input numbers that are multiples of 3" do
+        expected_output = (3..100).step(3).reject { |n| n % 5 == 0 }
+        expected_output.each do |value|
           expect(FizzBuzz.print(value)).to eq("Fizz")
         end
       end
     end
 
-    context "should return Buzz if it's a multiple of 5 skipping multiple of 3" do
+    context "when input is a multiple of 5 and not a multiple of 3" do
       it "should be eq Buzz" do
         expect(FizzBuzz.print(5)).to eq("Buzz")
+        expect(FizzBuzz.print(10)).to eq("Buzz")
+        expect(FizzBuzz.print(100)).to eq("Buzz")
       end
 
-      it "should be eq Buzz" do
-        5.step(100, 5) do |value|
-          next if value % 3 == 0
-
+      it "should be eq Buzz for input numbers that are multiples of 5" do
+        expected_output = (5..100).step(5).reject { |n| n % 3 == 0 }
+        expected_output.each do |value|
           expect(FizzBuzz.print(value)).to eq("Buzz")
         end
       end
     end
 
-    context "should return FizzBuzz if it's a multiple of 3 and 5 at same time" do
+    context "when input is a multiple of both 3 and 5" do
       it "should be eq FizzBuzz" do
         expect(FizzBuzz.print(15)).to eq("FizzBuzz")
+        expect(FizzBuzz.print(30)).to eq("FizzBuzz")
+        expect(FizzBuzz.print(60)).to eq("FizzBuzz")
       end
 
-      it "should be eq FizzBuzz" do
-        1.upto(100) do |value|
-          next unless (value % 3 == 0) && (value % 5 == 0)
-
+      it "should be eq FizzBuzz for input numbers that are multiples of both 3 and 5" do
+        expected_output = (1..100).select { |n| n % 3 == 0 && n % 5 == 0 }
+        expected_output.each do |value|
           expect(FizzBuzz.print(value)).to eq("FizzBuzz")
         end
       end
